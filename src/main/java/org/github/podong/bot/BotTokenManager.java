3493c442de5c2e9fe610d56082b9b806ff395a02
벨라 <icomksh@gmail.com>
2025-02-27 14:57:32 +0900

    lombok 추가

--- src/main/java/org/github/podong/bot/BotTokenManager.java
+++ src/main/java/org/github/podong/bot/BotTokenManager.java
@@ -1,15 +1,14 @@
 package org.github.podong.bot;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@Getter
 @Component
 public class BotTokenManager {
 
     @Value("${discord.token}")
     private String discordBotToken;
 
-    public String getDiscordBotToken() {
-        return discordBotToken;
-    }
 }
